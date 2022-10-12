c1fbf67993cfd9eaa5578a89b26df4530fb1c7b1
Filip Hrkać <filip.hrkac@testnisite.local>
2022-10-12 12:06:15 +0200

    editing login and register css

--- ui/src/Register/Register.js
+++ ui/src/Register/Register.js
@@ -1,86 +1,101 @@
-import { useState } from 'react';
-import { variables } from '../Variables';
+import { useState } from "react";
+import { variables } from "../Variables";
 import Cookies from "universal-cookie";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
+import classes from "./Register.module.css";
 export default function Register() {
+  const [username, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-const [username, setName] = useState('');
-const [password, setPassword] = useState('');
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
 
-// Handling the name change
-const handleName = (e) => {
-  setName(e.target.value);
-};
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-// Handling the password change
-const handlePassword = (e) => {
-  setPassword(e.target.value);
-};
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
+  let cookies = new Cookies();
 
-let cookies = new Cookies();
+  // Handling the form submission
+  const handleSubmit = (e) => {
+    fetch(variables.API_URL + "users/register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          cookies.set("id", result.id, { path: "/" });
+          cookies.set("username", result.username, { path: "/" });
+          cookies.set("role", result.role, { path: "/" });
 
-// Handling the form submission
-const handleSubmit = (e) => {
-      fetch(variables.API_URL + 'users/register', {
-          method:'POST',
-          headers: {
-              'Accept':'application/json',
-              'Content-Type':'application/json'
-          },
-          body:JSON.stringify({
-              username: username,
-              password: password,
-          })
-      })
-      .then(res=>res.json())
-      .then((result)=>{
-        console.log(result);
-        cookies.set("id", result.id, { path: '/' });
-        cookies.set("username", result.username, { path: '/' });
-        cookies.set("role", result.role, { path: '/' });
-
-        if (result.role !== undefined) {
-          navigate("/movies");
-        }
-        else {
-          window.location.reload();
-        }
-      },(error)=>{
+          if (result.role !== undefined) {
+            navigate("/movies");
+          } else {
+            window.location.reload();
+          }
+        },
+        (error) => {
           console.log(error);
-      })
+        }
+      );
 
     console.log(username);
     e.preventDefault();
-  if (username === '' || password === '') {
-      console.log('err');
+    if (username === "" || password === "") {
+      console.log("err");
     } else {
-      console.log('success');
-  }
-};
+      console.log("success");
+    }
+  };
 
-return (
-  <div className="form">
-  <div>
-    <h1>User Registration</h1>
-  </div>
-
-	<form>
-		{/* Labels and inputs for form data */}
-		<label className="label">Name</label>
-		<input onChange={handleName} className="input"
-		value={username} type="text" />
-
-		<label className="label">Password</label>
-		<input onChange={handlePassword} className="input"
-		value={password} type="password" />
-
-		<button onClick={handleSubmit} className="btn" type="submit">
-		  Submit
-		</button>
-	</form>
-	</div>
-);
+  return (
+    <div className={classes.registerContainer}>
+      <h3>REGISTER</h3>
+      <form onSubmit={handleSubmit}>
+        <div className={classes.username}>
+          <input
+            value={username}
+            type="text"
+            placeholder="Username"
+            onChange={handleName}
+          />
+        </div>
+        <div className={classes.password}>
+          <input
+            value={password}
+            type="password"
+            placeholder="Password"
+            onChange={handlePassword}
+          />
+        </div>
+        <div className={classes.moreOptions}>
+          <div className={classes.checkBox}>
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <a href="#">Forgot?</a>
+        </div>
+        <div className={classes.toLoginLink}>
+          <Link to="/">Already have an account?</Link>
+        </div>
+        <button type="submit" className={classes.submitBtn}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
